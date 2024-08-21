c9007f07c2e030464e5c6c8dcf378c5e6435c336
William Mitchell <will@Williams-Laptop.local>
2024-08-21 14:13:57 -0700

    Added a simple test

--- test_generate_sets.py
+++ test_generate_sets.py
@@ -6,3 +6,24 @@ def test_generate_sets():
     card =  Card(id=1,shape=1, color=1, shade=1, number=1)
     
     assert True == True
+    
+
+def test_single_card():
+    # No valid or invalid sets
+    test_data = [
+        
+        Card(id=1,shape=3, color=3, shade=3, number=3),
+        
+    ]
+    expected_valid = [
+    ]
+    
+    expected_invalid = [
+    ]
+    
+    valid, invalid  = generate_all_sets(test_data)
+    assert valid == expected_valid
+    assert invalid == expected_invalid
+    
+        
+    
